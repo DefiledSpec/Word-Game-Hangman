8bb4b1123c386b7e2b5759ba930d72b99a819530
DefiledSpec <defiledspec@gmail.com>
2018-05-29 18:40:29 -0500

    finalized game logic, added html UI, cleaned up console logs and comments in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,73 +1,119 @@
 const log = console.log;
 let score = 0;
+let streak = 0;
+let guessesLeft = 11;
+
+let started = false;
 
 let userInput = [];
-let userGuesses = userInput.length;
 
+let unsolvedDiv = $('#unsolved');
+let userInputDiv = $('#userInput');
+let userScoreDiv = $('#userScore');
+let userStreakDiv = $('#userStreak');
+let startMessageDiv = $('#startMessage');
+let guessesLeftDiv = $('#guessesLeft');
 
-let beenUsed = a => {
+let word;
+let unsolved;
+let words = [
+    'blue',
+    'yellow',
+    'red',
+    'green',
+    'orange',
+    'teal',
+    'gray',
+    'magenta',
+    'purple',
+    'pink'
+]
+
+function pickWord() { //picks a random word and initializes the array
+    let choice = words[Math.floor(Math.random() * words.length)];
+    word = [];
+    unsolved = [];
+    userInput = [];
+    
+    for(let i = 0; i < choice.length; i++) {
+        word.push(choice[i]);
+        unsolved.push('_');
+    }
+    unsolvedDiv.text(unsolved.join(' '));
+    userInputDiv.text(userInput.join(' '));
+    userScoreDiv.text(score);
+    userStreakDiv.text(streak);
+    guessesLeftDiv.text(guessesLeft);
+    //log(word)  
+}
+
+let beenUsed = a => { //bool to determine if a letter has already been used
     let usedLetter = userInput.indexOf(a);
     return (usedLetter >= 0) ? true : false;
 }
 
-let word = ['y', 'e', 'l', 'l', 'o', 'w'];
-let word2 = ['y', 'e', 'l', 'l', 'o', 'w'];
-let unsolved = ['_', '_', '_', '_', '_', '_'];
-
-function checkComplete() {
+function checkComplete() { //checks if the word has been completed
     let startWord = word.toString();
-    let unsolvedWord = word2.toString();
+    let unsolvedWord = unsolved.toString();
     if(startWord === unsolvedWord){ return true } else { return false }
 }
 
-function checkForLetter(Arr, char) {
-    let checked = []; 
-    if(beenUsed(char)) { 
+function checkForLetter(Arr, char) { //check if users letter exists in word[]
+    let checked = [];
+    let userGuesses = userInput.length;
+    if(userGuesses === 11 && !checkComplete()) {
+        score = 0;
+        streak = 0;
+        guessesLeft = 11;
+        pickWord();
+        userInputDiv.text(userInput.join(' '));
+        unsolvedDiv.text(unsolved.join(' '));
+        userScoreDiv.text(score);
+        return alert('Too many guesses! Better luck next time!');
+    }
+    if(beenUsed(char)) { //check if letter has been used already
         return log(`'${char}' has already been used! ${userInput.join(' ')}`);
     }else{
         userInput.push(char);
-        log(`User Input: ${userInput}`);
+        // log(`User Input: ${userInput}`);
         Arr.forEach((ltr, idx) => {
             if(ltr === char) {
                 unsolved.splice(idx, 1, char);
                 score++;
             }
         });  
-    } 
+    }
+    guessesLeftDiv.text(guessesLeft);
+    userInputDiv.text(userInput.join(' '));
+    unsolvedDiv.text(unsolved.join(' '));
+    userScoreDiv.text(score);
     
-    log(unsolved);
-    log(score);
 }
 
-function game(char) {
-    let done = checkComplete();
-    log(done);
-    //before we check a letter lets make sure that it hasn't already been used as to not keep incrimenting the score.
+function game(char) { //this runs the game from the event handler
+    guessesLeft--;
     let wordChecked = checkForLetter(word, char); // an array of booleans indicating whether the word contains a specific letter
-    // log(wordChecked);
+    let done = checkComplete();
+    if(done) {
+        log('You Win!');
+        guessesLeft = 11;
+        streak++;
+        pickWord();
+    }
 }
 
-
-let started = false; // this doesn't work set to false to reset the game
-
-
-log('Press any key to get started!');
-
-document.onkeyup = function (e) {
+document.onkeyup = function (e) { //event handler
     let key = e.key.toLowerCase();
     let code = e.keyCode;
-
     if(started) {
         if(code > 64 && code < 91) { //check if the key is a letter a-z
-            // log(`${key} ${code} is a letter`);
             game(key);
         } else {
             log(`${key} ${code} is not a letter`);
         }
-    }else{
-        log(`Game started.`);
-        log(unsolved)
+    }else{        
+        startMessageDiv.text('')
         started = true;
+        pickWord();
     }
 };
-
